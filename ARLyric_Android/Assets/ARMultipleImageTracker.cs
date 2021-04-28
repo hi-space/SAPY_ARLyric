f973c338a655c82e16187fd77ee965ce2c7dc5da
hi-space <hi-space.j@gmail.com>
2021-04-28 17:02:47 +0900

    multiple image tracking

---
+++ ARLyric_Android/Assets/ARMultipleImageTracker.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class ARMultipleImageTracker : MonoBehaviour
+{
+    public ARTrackedImageManager trackedImageManager;
+    public GameObject[] placeablePrefabs;
+
+    private Dictionary<string, GameObject> spawnedPrefabs = new Dictionary<string, GameObject>();
+
+    void Start()
+    {
+        foreach (GameObject prefab in placeablePrefabs)
+        {
+            GameObject newPrefab = Instantiate(prefab, Vector3.zero, Quaternion.identity);
+            newPrefab.name = prefab.name;
+            spawnedPrefabs.Add(newPrefab.name, newPrefab);
+            newPrefab.SetActive(false);
+        }
+    }
+
+    private void OnEnable()
+    {
+        trackedImageManager.trackedImagesChanged += OnTrackedImagesChanged;
+    }
+
+    private void OnDisable()
+    {
+        trackedImageManager.trackedImagesChanged -= OnTrackedImagesChanged;
+    }
+
+    private void OnTrackedImagesChanged(ARTrackedImagesChangedEventArgs eventArgs)
+    {
+        foreach (ARTrackedImage trackedImage in eventArgs.added)
+        {
+            UpdateImage(trackedImage);
+        }
+        foreach (ARTrackedImage trackedImage in eventArgs.updated)
+        {
+            UpdateImage(trackedImage);
+        }
+        foreach (ARTrackedImage trackedImage in eventArgs.removed)
+        {
+            spawnedPrefabs[trackedImage.name].SetActive(false);
+        }
+    }
+
+    private void UpdateImage(ARTrackedImage trackedImage)
+    {
+        string name = trackedImage.referenceImage.name;
+
+        spawnedPrefabs[name].SetActive(true);
+        spawnedPrefabs[name].transform.position = trackedImage.transform.position;
+        spawnedPrefabs[name].transform.rotation = trackedImage.transform.rotation;
+    }
+}
