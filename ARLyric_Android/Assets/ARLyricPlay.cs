0cc0df89348d941fe5deac8dbf688d7ebd0503b4
hi-space <hi-space.j@gmail.com>
2021-04-28 18:27:17 +0900

    AR Lyric with Plane Tracking

---
+++ ARLyric_Android/Assets/ARLyricPlay.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+
+public class ARLyricPlay : MonoBehaviour
+{
+    public GameObject[] objectPrefab;
+    public AudioClip[] audioPrefab;
+
+    ARRaycastManager raycastManager;
+
+    List<GameObject> placedObject = new List<GameObject>();
+    
+    AudioSource currentAudio;
+
+    int index;
+
+    void Start()
+    {
+        raycastManager = GetComponent<ARRaycastManager>();
+        currentAudio = GetComponent<AudioSource>();
+
+        foreach (GameObject obj in objectPrefab)
+        {
+            GameObject newObj = Instantiate(obj, Vector3.zero, Quaternion.identity);
+            newObj.SetActive(false);
+            placedObject.Add(newObj);
+        }
+
+        index = 0;
+    }
+
+    void Update()
+    {
+        if (!currentAudio.isPlaying)
+        {
+            index = index >= objectPrefab.Length ? 0 : index + 1;
+
+            currentAudio.clip = audioPrefab[index];
+            currentAudio.Play();
+        }
+
+        if (Input.touchCount > 0)
+        {
+            List<ARRaycastHit> hits = new List<ARRaycastHit>();
+            if (raycastManager.Raycast(Input.GetTouch(0).position, hits, TrackableType.All))
+            {
+                Pose hitPose = hits[0].pose;
+
+                foreach (GameObject obj in placedObject)
+                {
+                    obj.SetActive(false);
+                }
+
+                GameObject currentObject = placedObject[index];
+                currentObject.transform.position = hitPose.position;
+                currentObject.transform.rotation = hitPose.rotation;
+                currentObject.SetActive(true);
+            }            
+        }
+    }
+}
